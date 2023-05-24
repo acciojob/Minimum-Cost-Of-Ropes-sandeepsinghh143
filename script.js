f71250d09249b2e477acc6bcf3d0ebce6cb75544
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-24 10:51:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function calculateMinCost() {
   //your code here
+  let sumAll=0;;
   let sum=0;
 	for(let i=0;i<arr.length;i++){
 		sum+=arr[i];
+		sumAll+=sum;
 	}
-  return sum;
+  return sumAll;
   
 }  
